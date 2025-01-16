802b5fd513b0b47bf9b78dd594313d2eba82983f
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-01-16 16:39:20 +0100

    in progress

--- src/main/scala/Forth.scala
+++ src/main/scala/Forth.scala
@@ -1,6 +1,16 @@
 import ForthError.ForthError
 
+class Forth extends ForthEvaluator:
+   def eval(text: String): Either[ForthError, ForthEvaluatorState] =
+      val words = text.split(" ").toList
+      parse(words)
 
-class Forth extends ForthEvaluator {
-  def eval(text: String): Either[ForthError, ForthEvaluatorState] = ???
-}
+   private def parse(strings: List[String]): Either[ForthError, ForthEvaluatorState] =
+      if strings.isEmpty then Left(ForthError.StackUnderflow)
+      else
+         val numbers: List[Int] = strings.takeWhile(_.matches("\\d")).map(_.toInt)
+         val st = numbers
+         Right(
+           new ForthEvaluatorState:
+              override val state: List[Int] = st
+         )
