5b86abd7b9a7372a5a43a8699be224bdd5edf266
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-02-06 16:54:04 +0100

    new idea with mutable Stack and Map

--- src/main/scala/Forth.scala
+++ src/main/scala/Forth.scala
@@ -1,6 +1,25 @@
 import ForthError.ForthError
+import scala.collection.mutable
 
+class Forth extends ForthEvaluator:
+   private val stack: mutable.Stack[Int] = mutable.Stack()
+   private val dictionary: mutable.Map[String, List[String]] = mutable.Map(
+     "+" -> List("add"),
+     "-" -> List("subtract"),
+     "*" -> List("multiply"),
+     "/" -> List("divide"),
+     "dup" -> List("dup"),
+     "drop" -> List("drop"),
+     "swap" -> List("swap"),
+     "over" -> List("over")
+   )
+   def eval(text: String): Either[ForthError, ForthEvaluatorState] = ???
 
-class Forth extends ForthEvaluator {
-  def eval(text: String): Either[ForthError, ForthEvaluatorState] = ???
-}
+   private def add() = ???
+   private def subtract() = ???
+   private def multiply() = ???
+   private def divide() = ???
+   private def dup() = ???
+   private def drop() = ???
+   private def swap() = ???
+   private def over() = ???
