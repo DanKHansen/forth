087159797f151ff0ecc9043a7633ef91154f67a9
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-02-07 15:37:46 +0100

    in progress

--- src/main/scala/Forth.scala
+++ src/main/scala/Forth.scala
@@ -1,25 +1,62 @@
 import ForthError.ForthError
-import scala.collection.mutable
+import scala.util.{Failure, Success, Try}
+
+class State extends ForthEvaluatorState:
+   var stack: List[Int] = List[Int]()
+   private val udfWs: Map[String, String] = Map.empty
+   def getInputString(name: String): String = udfWs(name)
+
+   override def toString: String = stack.reverse.mkString(" ")
 
 class Forth extends ForthEvaluator:
-   private val stack: mutable.Stack[Int] = mutable.Stack()
-   private val dictionary: mutable.Map[String, List[String]] = mutable.Map(
-     "+" -> List("add"),
-     "-" -> List("subtract"),
-     "*" -> List("multiply"),
-     "/" -> List("divide"),
-     "dup" -> List("dup"),
-     "drop" -> List("drop"),
-     "swap" -> List("swap"),
-     "over" -> List("over")
-   )
-   def eval(text: String): Either[ForthError, ForthEvaluatorState] = ???
+   def eval(text: String): Either[ForthError, ForthEvaluatorState] =
+      val state = new State
+      evaluate(state, text.toLowerCase())
 
-   private def add() = ???
-   private def subtract() = ???
-   private def multiply() = ???
-   private def divide() = ???
-   private def dup() = ???
-   private def drop() = ???
-   private def swap() = ???
-   private def over() = ???
+   def evaluate(st: State, input: String): Either[ForthError, State] =
+      input
+         .split(" ")
+         .foldLeft[Either[ForthError, State]](Right(st))((e, str) =>
+            e match
+               case Right(s) => run(s, str)
+               case Left(_)  => e)
+
+   private def run(st: State, word: String): Either[ForthError, State] =
+      word match
+         case w if w.forall(_.isDigit) =>
+            st.stack = w.toInt :: st.stack
+            Right(st)
+         case "+"                      => arithmetic(st, (a, b) => a + b)
+         case "-"                      => arithmetic(st, (a, b) => b - a)
+         case "*"                      => arithmetic(st, (a, b) => a * b)
+         case "/"                      => arithmetic(st, (a, b) => b / a)
+         case "dup" | "drop"           => stackOps(st, word, 1)
+         case "swap" | "over"          => stackOps(st, word, 2)
+         case _                        => Left(ForthError.UnknownWord)
+
+   private def arithmetic(st: State, f: (Int, Int) => Int): Either[ForthError, State] =
+      if st.stack.size < 2 then Left(ForthError.StackUnderflow)
+      else
+         val res = Try(st.stack.take(2).reduce(f))
+         res match
+            case Failure(_)     => Left(ForthError.DivisionByZero)
+            case Success(value) =>
+               st.stack = value :: st.stack.drop(2)
+               Right(st)
+
+   private def stackOps(st: State, word: String, minSize: Int): Either[ForthError, State] =
+      if st.stack.size < minSize then Left(ForthError.StackUnderflow)
+      else
+         word match
+            case "dup"  =>
+               st.stack = st.stack.head :: st.stack
+            case "drop" =>
+               st.stack = st.stack.tail
+            case "swap" =>
+               st.stack = st.stack(1) :: st.stack.head :: st.stack.drop(2)
+            case "over" =>
+               st.stack = st.stack(1) :: st.stack.head :: st.stack(1) :: st.stack.drop(2)
+         Right(st)
+
+   def udfWords(st: State, word: String): Either[ForthError, State] =
+      evaluate(st, st.getInputString(word))
