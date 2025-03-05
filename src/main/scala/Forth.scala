48e1ad657d6321bdfc001350a85e910b5061b4b3
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-03-05 13:08:30 +0100

    solution

--- src/main/scala/Forth.scala
+++ src/main/scala/Forth.scala
@@ -1,16 +1,12 @@
-import ForthError.ForthError
-
 import scala.annotation.tailrec
-
-type Result = Either[ForthError, ForthEvaluatorState]
+type Result = Either[ForthError.Value, ForthEvaluatorState]
 
 case class State(stack: List[Int]) extends ForthEvaluatorState:
    override def toString: String = super.toString
 
 class Forth extends ForthEvaluator:
    def eval(text: String): Result =
-      val words = text.toLowerCase.split(" ").toList
-      execute(words, Right(State(Nil)))
+      execute(text.toLowerCase.split(" ").toList, Right(State(Nil)))
 
 object Add extends Definition:
    override def evaluate(state: Result): Result =
@@ -99,29 +95,33 @@ case class Num(num: Int) extends Definition:
          case Left(value) => Left(value)
          case Right(st)   => Right(State(num :: st.stack))
 
-def Udf(l: List[String]): Result =
-   val ws = l.splitAt(l.indexOf(";"))
-   val wrd = ws._1.tail
-   val rest = wrd.tail
-   println(ws)
-   println(wrd)
-   println(rest)
-   execute(rest, Right(State(Nil)))
+def udf(cs: List[String], st: Result, dict: Map[String, String]): Result =
+   val ws = cs.map(_.toLowerCase).takeWhile(_ != ";").drop(1)
+   if ws.head.forall(_.isDigit) then Left(ForthError.InvalidWord)
+   else
+      val newDict = dict + (ws.head -> ws.tail
+         .map(word => dict.getOrElse(word, word))
+         .mkString(" "))
+      val newWS = cs.diff(ws).drop(2)
+      execute(newWS, st, newDict)
 
 @tailrec
-def execute(ws: List[String], st: Result): Result =
-   // println(ws)
-   if ws.isEmpty then st
-   else
-      ws match
-         case num :: tail if num.forall(_.isDigit) => execute(tail, Num(num.toInt).evaluate(st))
-         case ::(head, next) if head == "+"        => execute(next, Add.evaluate(st))
-         case ::(head, next) if head == "-"        => execute(next, Sub.evaluate(st))
-         case ::(head, next) if head == "*"        => execute(next, Mul.evaluate(st))
-         case ::(head, next) if head == "/"        => execute(next, Div.evaluate(st))
-         case ::(head, next) if head == "dup"      => execute(next, Dup.evaluate(st))
-         case ::(head, next) if head == "drop"     => execute(next, Drop.evaluate(st))
-         case ::(head, next) if head == "swap"     => execute(next, Swap.evaluate(st))
-         case ::(head, next) if head == "over"     => execute(next, Over.evaluate(st))
-         case ::(head, _) if head == ":"           => Udf(ws)
-         case _                                    => Left(ForthError.InvalidWord)
+def execute(ws: List[String], st: Result, dict: Map[String, String] = Map().empty): Result =
+   ws.headOption match
+      case Some(num) if num.forall(_.isDigit) => execute(ws.tail, Num(num.toInt).evaluate(st), dict)
+      case Some(w)                            =>
+         dict.get(w) match
+            case Some(s) => execute(s.split(" ").toList ::: ws.tail, st, dict)
+            case None    =>
+               w match
+                  case "+"    => execute(ws.tail, Add.evaluate(st), dict)
+                  case "-"    => execute(ws.tail, Sub.evaluate(st), dict)
+                  case "*"    => execute(ws.tail, Mul.evaluate(st), dict)
+                  case "/"    => execute(ws.tail, Div.evaluate(st), dict)
+                  case "dup"  => execute(ws.tail, Dup.evaluate(st), dict)
+                  case "drop" => execute(ws.tail, Drop.evaluate(st), dict)
+                  case "swap" => execute(ws.tail, Swap.evaluate(st), dict)
+                  case "over" => execute(ws.tail, Over.evaluate(st), dict)
+                  case ":"    => udf(ws, st, dict)
+                  case _      => Left(ForthError.UnknownWord)
+      case None                               => st
