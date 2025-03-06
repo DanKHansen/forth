e933d37d28450eec2d0375f524872018c2148d86
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-03-06 10:03:54 +0100

    refactoring

--- src/main/scala/Forth.scala
+++ src/main/scala/Forth.scala
@@ -8,120 +8,74 @@ class Forth extends ForthEvaluator:
    def eval(text: String): Result =
       execute(text.toLowerCase.split(" ").toList, Right(State(Nil)))
 
-object Add extends Definition:
-   override def evaluate(state: Result): Result =
+sealed trait Operation
+case object Add extends Operation
+case object Sub extends Operation
+case object Mul extends Operation
+case object Div extends Operation
+case object Dup extends Operation
+case object Swap extends Operation
+case object Over extends Operation
+case object Drop extends Operation
+
+object Def extends Definition:
+   case class Num(num: Int) extends Definition:
+      override def evaluate(state: Result): Result =
+         state match
+            case Left(value) => Left(value)
+            case Right(st)   => Right(State(num :: st.stack))
+   def eval(state: Result, op: Operation): Result =
       state match
-         case Left(value) => Left(value)
+         case Left(error) => Left(error)
          case Right(st)   =>
             st.stack match
-               case l if l.length < 2      => Left(ForthError.StackUnderflow)
-               case head :: follow :: next => Right(State((head + follow) :: next))
-               case _                      => Left(ForthError.InvalidWord)
+               case Nil                    => Left(ForthError.StackUnderflow)
+               case head :: follow :: rest =>
+                  op match
+                     case Add  => Right(State(head + follow :: rest))
+                     case Sub  => Right(State(follow - head :: rest))
+                     case Mul  => Right(State(follow * head :: rest))
+                     case Div  =>
+                        if head == 0 then Left(ForthError.DivisionByZero)
+                        else Right(State(follow / head :: rest))
+                     case Dup  => Right(State(head :: head :: follow :: rest))
+                     case Swap => Right(State(follow :: head :: rest))
+                     case Over => Right(State(follow :: head :: follow :: rest))
+                     case Drop => Right(State(follow :: rest))
+               case head :: Nil            =>
+                  op match
+                     case Dup  => Right(State(head :: head :: Nil))
+                     case Drop => Right(State(Nil))
+                     case _    => Left(ForthError.StackUnderflow)
 
-object Sub extends Definition:
    override def evaluate(state: Result): Result =
-      state match
-         case Left(value) => Left(value)
-         case Right(st)   =>
-            st.stack match
-               case l if l.length < 2      => Left(ForthError.StackUnderflow)
-               case head :: follow :: next => Right(State((follow - head) :: next))
-               case _                      => Left(ForthError.InvalidWord)
+      Left(ForthError.InvalidWord)
 
-object Mul extends Definition:
-   override def evaluate(state: Result): Result =
-      state match
-         case Left(value) => Left(value)
-         case Right(st)   =>
-            st.stack match
-               case l if l.length < 2      => Left(ForthError.StackUnderflow)
-               case head :: follow :: next => Right(State((head * follow) :: next))
-               case _                      => Left(ForthError.InvalidWord)
-
-object Div extends Definition:
-   override def evaluate(state: Result): Result =
-      state match
-         case Left(value) => Left(value)
-         case Right(st)   =>
-            st.stack match
-               case 0 :: _                 => Left(ForthError.DivisionByZero)
-               case head :: follow :: next => Right(State((follow / head) :: next))
-               case l if l.length < 2      => Left(ForthError.StackUnderflow)
-               case _                      => Left(ForthError.InvalidWord)
-
-object Dup extends Definition:
-   override def evaluate(state: Result): Result =
-      state match
-         case Left(value) => Left(value)
-         case Right(st)   =>
-            st.stack match
-               case l if l.length < 1 => Left(ForthError.StackUnderflow)
-               case head :: next      => Right(State(head :: head :: next))
-               case _                 => Left(ForthError.InvalidWord)
-
-object Swap extends Definition:
-   override def evaluate(state: Result): Result =
-      state match
-         case Left(value) => Left(value)
-         case Right(st)   =>
-            st.stack match
-               case l if l.length < 1      => Left(ForthError.StackUnderflow)
-               case head :: follow :: next => Right(State(follow :: head :: next))
-               case _                      => Left(ForthError.InvalidWord)
-
-object Over extends Definition:
-   override def evaluate(state: Result): Result =
-      state match
-         case Left(value) => Left(value)
-         case Right(st)   =>
-            st.stack match
-               case l if l.length < 1      => Left(ForthError.StackUnderflow)
-               case head :: follow :: next => Right(State(follow :: head :: follow :: next))
-               case _                      => Left(ForthError.InvalidWord)
-
-object Drop extends Definition:
-   override def evaluate(state: Result): Result =
-      state match
-         case Left(value) => Left(value)
-         case Right(st)   =>
-            st.stack match
-               case l if l.length < 1 => Left(ForthError.StackUnderflow)
-               case _ :: next         => Right(State(next))
-               case _                 => Left(ForthError.InvalidWord)
-
-case class Num(num: Int) extends Definition:
-   override def evaluate(state: Result): Result =
-      state match
-         case Left(value) => Left(value)
-         case Right(st)   => Right(State(num :: st.stack))
-
-def udf(cs: List[String], st: Result, dict: Map[String, String]): Result =
-   val ws = cs.map(_.toLowerCase).takeWhile(_ != ";").drop(1)
-   if ws.head.forall(_.isDigit) then Left(ForthError.InvalidWord)
+def udf(text: List[String], st: Result, dict: Map[String, String]): Result =
+   val ws = text.map(_.toLowerCase).takeWhile(_ != ";").drop(1)
+   if ws.isEmpty || ws.head.forall(_.isDigit) then Left(ForthError.InvalidWord)
    else
-      val newDict = dict + (ws.head -> ws.tail
-         .map(word => dict.getOrElse(word, word))
-         .mkString(" "))
-      val newWS = cs.diff(ws).drop(2)
+      val newDict = dict + (ws.head -> ws.tail.map(w => dict.getOrElse(w, w)).mkString(" "))
+      val newWS = text.diff(ws).drop(2)
       execute(newWS, st, newDict)
 
 @tailrec
 def execute(ws: List[String], st: Result, dict: Map[String, String] = Map().empty): Result =
    ws.headOption match
-      case Some(num) if num.forall(_.isDigit) => execute(ws.tail, Num(num.toInt).evaluate(st), dict)
-      case Some(w)                            =>
+      case None                           => st
+      case Some(n) if n.forall(_.isDigit) => execute(ws.tail, Def.Num(n.toInt).evaluate(st), dict)
+      case Some(w)                        =>
          dict.get(w) match
             case Some(s) => execute(s.split(" ").toList ::: ws.tail, st, dict)
             case None    =>
                w match
-                  case "+"    => execute(ws.tail, Add.evaluate(st), dict)
-                  case "-"    => execute(ws.tail, Sub.evaluate(st), dict)
-                  case "*"    => execute(ws.tail, Mul.evaluate(st), dict)
-                  case "/"    => execute(ws.tail, Div.evaluate(st), dict)
-                  case "dup"  => execute(ws.tail, Dup.evaluate(st), dict)
-                  case "drop" => execute(ws.tail, Drop.evaluate(st), dict)
-                  case "swap" => execute(ws.tail, Swap.evaluate(st), dict)
-                  case "over" => execute(ws.tail, Over.evaluate(st), dict)
+                  case "+"    => execute(ws.tail, Def.eval(st, Add), dict)
+                  case "-"    => execute(ws.tail, Def.eval(st, Sub), dict)
+                  case "*"    => execute(ws.tail, Def.eval(st, Mul), dict)
+                  case "/"    => execute(ws.tail, Def.eval(st, Div), dict)
+                  case "dup"  => execute(ws.tail, Def.eval(st, Dup), dict)
+                  case "drop" => execute(ws.tail, Def.eval(st, Drop), dict)
+                  case "swap" => execute(ws.tail, Def.eval(st, Swap), dict)
+                  case "over" => execute(ws.tail, Def.eval(st, Over), dict)
                   case ":"    => udf(ws, st, dict)
                   case _      => Left(ForthError.UnknownWord)
-      case None                               => st
